98f339ba2a57fc9079fbf8160c9c2db092cca59a
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2016-04-23 00:08:26 +0300

    Added cleanup script

--- provision.sh
+++ provision.sh
@@ -5,11 +5,16 @@ source /usr/local/bin/virtualenvwrapper.sh
 mkvirtualenv todo-django
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
 
+if [[ -z "$PROJECT_DIR" ]]; then
+    echo "ERROR: PROJECT_DIR env is empty"
+    exit 1
+fi
+
 # Install python requirements and run migrations
-cd /vagrant/todomvc-django-backbone/todomvc-django
+cd "$PROJECT_DIR"/todomvc-django
 pip install -r REQUIREMENTS
 python manage.py migrate --run-syncd
 
 # Install js requirements
-cd /vagrant/todomvc-django-backbone/todomvc-backbone
+cd "$PROJECT_DIR"/todomvc-backbone
 bower install
